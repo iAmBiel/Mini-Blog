beb08615d94e68b2aa5d53d5280cbcc969112132
iAmBiel <bielvm02@gmail.com>
2023-12-19 13:03:20 -0300

    final adjusts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,68 @@
 import './App.css';
 
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { onAuthStateChanged } from "firebase/auth";
+
+//hooks
+import { useState, useEffect } from "react";
+import { useAuthentication } from "./hooks/useAuthentication";
+
+//context
+import {AuthProvider} from "./contexts/AuthContext";
+
+//pages
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import CreatePost from "./pages/CreatePost/CreatePost";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Search from './pages/Search/Search';
+import Post from './pages/Post/Post';
+
+//components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+
+
 function App() {
+
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
+
+  const loadingUser = user === undefined;
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, [auth]);
+
+  if (loadingUser) {
+    return <p>Carregando...</p>;
+  }
+
   return (
     <div className="App">
-      <h1>MiniBlog</h1>
+      <AuthProvider value={{ user }}>
+        <BrowserRouter>
+          <Navbar />
+          <div className='container'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/search' element={<Search />} />
+              <Route path='/posts/:id' element={<Post />} />
+              <Route path='/login' element={!user ? <Login /> : <Navigate to="/" />} />
+              <Route path='/register' element={!user ? <Register /> : <Navigate to="/" />} />
+              <Route path='/posts/create' element={user ? <CreatePost /> : <Navigate to="/login" />} />
+              <Route path='/dashboard' element={user ? <Dashboard /> : <Navigate to="/login" />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
